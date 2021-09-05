d960be1d9f7c4f83b2ca9b44dbc4ce1b1e422934
diegoroyo <diego.roymen@gmail.com>
2021-09-05 17:04:36 +0200

    feat: add new Spectrum variant: TimedColor (RGB Color + time component)
    * time is just a number w/o units for now, will change later
    * added is_transient_v and other utilities for new variant

--- include/mitsuba/core/fwd.h
+++ include/mitsuba/core/fwd.h
@@ -60,6 +60,7 @@ template <typename Value, size_t Size>          struct Vector;
 template <typename Value, size_t Size>          struct Point;
 template <typename Value, size_t Size>          struct Normal;
 template <typename Value, size_t Size>          struct Color;
+template <typename Value, size_t Size>          struct TimedColor;
 template <typename Value, size_t Size>          struct Spectrum;
 template <typename Point>                       struct Transform;
 template <typename Point, typename Spectrum>    struct Ray;
